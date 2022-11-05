e5c9c61ced6e9193fc3fe6b808fc0b67a79bdd54
pavxn <90316177+pavxn@users.noreply.github.com>
2022-11-05 12:26:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -97,10 +97,10 @@ async def delete_user(user_id : str):
     raise HTTPException(500,
     f"Delete could not be executed.")
     
-
+    
 @app.get("/login/", response_model=User)
 async def get_user_by_id(user_id : str):  
-    response = await get_one_user(user_id)
+    response = await get_one_user_by_email(user_id)
     if response:
         return response
 
